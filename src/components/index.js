85605f83cfbe8f71e918bb6e4bfdc0c5794d9d69
Garrett Truong <aznkungfubunnee@gmail.com>
2022-09-09 11:27:16 -0500

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -29,4 +29,6 @@ export {default as UpdatePublicItem} from './UpdatePublicItem'
 
 export {default as OrderHistory} from './OrderHistory'
 export {default as OrderHistoryItems} from './OrderHistoryItems'
+export {default as ProductGrind} from './ProductGrind'
+
 
